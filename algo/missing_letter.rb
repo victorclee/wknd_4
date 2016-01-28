1014215634f01f56b5082193e5cc58eb182e8e3e
Josh Nixon <seriousfools@gmail.com>
2016-01-28 13:29:27 -0600

    Update missing_letter.rb

--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -1,6 +1,6 @@
 # Return the missing letter from a given range of letters passed into the method
 # as a string. If there is no missing letter, the method should return nil.
-# bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd"
+# bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
 def find_missing_letter(range)
 
