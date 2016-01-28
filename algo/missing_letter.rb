2cd4fef37277184ef67ebfe4a423a0352ac07bc2
Jay Wengrow <jaywngrw@gmail.com>
2016-01-27 22:58:45 -0600

    initial commit

---
+++ algo/missing_letter.rb
@@ -0,0 +1,34 @@
+# Return the missing letter from a given range of letters passed into the method
+# as a string. If there is no missing letter, the method should return nil.
+
+def find_missing_letter(range)
+
+end
+
+# Driver code - don't touch anything below this line.
+puts "TESTING find_missing_letter..."
+puts
+
+result = find_missing_letter("opqrsuv")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "t"
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = find_missing_letter("xyz")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == nil
+  puts "PASS!"
+else
+  puts "F"
+end
